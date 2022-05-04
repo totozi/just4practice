7ac7a7b02f6e8a547905fbfb63d7eeb01d503c01
totozi <skgydnjs@gmail.com>
2022-05-04 22:30:14 +0900

    기수 변환 but 수정 필요

--- src/main/java/data/structure/ch02/Ch02_3.java
+++ src/main/java/data/structure/ch02/Ch02_3.java
@@ -22,20 +22,21 @@ public class Ch02_3 {
 				System.out.println("어떤 진수로 변환할까요? (2~36) : ");
 				cd = scan.nextInt();
 			} while (cd < 2 || cd > 36);
-			dno = cardConvR(no, cd, cno);
+//			dno = cardConvR(no, cd, cno);
+			dno = cardConv(no, cd, cno);
 
 			System.out.print(cd + "진수로는 ");
-			for (int i = dno - 1; i >= 0; i--)
+//			for (int i = dno - 1; i >= 0; i--)
+			for (int i = 0; i <= dno; i++)
 				System.out.print(cno[i]);
 			System.out.println("입니다.");
 
 			System.out.println("한 번 더 할까요? (1.Y / 2.N) : ");
 			retry = scan.nextInt();
 		} while (retry == 1);
-		if(retry == 2) {
+		if (retry == 2) {
 			System.out.println("종료되었습니다.");
 		}
-
 	}
 
 	private static int cardConvR(int no, int cd, char[] cno) {
@@ -49,4 +50,37 @@ public class Ch02_3 {
 		return digits;
 	}
 
+	private static int cardConv(int no, int cd, char[] cno) {
+		int orgNo = no;
+		int digits = 0;
+		String dchar = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		
+		int rem =0;
+		do {
+
+			cno[digits++] = dchar.charAt(no % cd);
+			if (no != orgNo) {
+				System.out.println(no +" ... " + rem);
+				rem = no % cd;
+				no /= cd;
+			}
+			else {
+				System.out.println(no);
+				rem = no % cd;
+				no /= cd;
+			}
+			System.out.println("======");
+			
+		} while (no != 0);
+		System.out.println(no +" ... " + rem);
+		System.out.println("======");
+		char tmp;
+		for (int i = 0; i <= digits / 2; i++) {
+			tmp = cno[i];
+			cno[i] = cno[digits - i];
+			cno[digits - i] = tmp;
+		}
+		//TODO 12를 13진수로 하는 경우에 잘 안됨 원인파악 하고 수정할 것
+		return digits;
+	}
 }
